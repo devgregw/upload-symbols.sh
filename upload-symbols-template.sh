4ab3c0c394dc337f362ca4979f8bc0f059c16f57
Greg Whatley <me@gregwhatley.dev>
2021-03-07 22:33:19 -0600

    Update for FirebaseCrashlytics

--- upload-symbols-template.sh
+++ upload-symbols-template.sh
@@ -4,13 +4,15 @@
 #  Created by Greg Whatley on 1/11/19.
 #
 #  TEMPLATE ONLY
-#  COMPLETE LINES 13 AND 14 BEFORE USING
+#  COMPLETE LINES 15 AND 16 BEFORE USING
 #
 #  This script uploads dSYMs to Firebase for processing.
 #  Usage: Find the archive to upload dSYMs from and click "Download Debug Symbols" from Xcode Organizer.  Drag script into terminal then drag archive file (~/Library/Developer/Xcode/Archives) into terminal after script.  Press return.
 
+# Note: If you haven't migrated from Fabric Crashlytics, replace FirebaseCrashlytics to Fabric on line 15.
+
 ARCHIVE=$1
-UPLOADSCRIPT="...../Pods/Fabric/upload-symbols" # Path to Fabric upload script
+UPLOADSCRIPT="...../Pods/FirebaseCrashlytics/upload-symbols" # Path to Fabric upload script
 GOOGLECONFIG="...../GoogleService-Info.plist"   # Path to Google services info
 
 if [ -d $ARCHIVE ]; then
